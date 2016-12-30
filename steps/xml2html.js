869905dd84f1f59b34da05b89a9c6cdb33dbce14
Vindexus <vindexus@gmail.com>
2016-12-29 19:42:08 -0800

    Better slugify

--- steps/xml2html.js
+++ steps/xml2html.js
@@ -1,19 +1,10 @@
 var jsdom                     = require("jsdom");
-
+var slugify                   = require('slug')
 module.exports = function (content, name, config, done) {
   config = config || {}
   config.protectedNodeNames = config.protectedNodeNames || {}
   config.protectedNodeNames.push('body')
   config.protectedNodeNames.push('script')
-
-  function slugify(text)  {
-    return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
-  }
 
   jsdom.env(content,
     ["http://code.jquery.com/jquery.min.js"],
@@ -61,7 +52,7 @@ module.exports = function (content, name, config, done) {
       if(config.idsFromText && config.idsFromText.length > 0) {
         config.idsFromText.forEach(function (selector) {
           $(selector).each(function (index, el) {
-            $(el).attr('id', slugify($(el).text()))
+            $(el).attr('id', slugify($(el).text(), {lower: true}))
           })
         })
       }
